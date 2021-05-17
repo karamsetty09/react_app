0f0ee409f9387377182f3e7d8465ce0c6cff0877
venkata sandeep kumar karamsetty <venkatasandeepkumarkaramsetty@gmail.com>
2021-05-17 15:30:15 +1000

    Adding Footer, Main and Footer to app.js with props usage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+function Header(props){
+  return(
+    <header>
+      <h1>{props.name}'s Portfolio</h1>
+    </header>
+  );
+}
+
+function Main(props){
+  return(
+  <main>
+    <h3>Welcome to my {props.adjective} Portfolio</h3>
+  </main>
+  );
+}
+
+function Footer(props){
+  return(
+    <footer>
+      <h6> Copyright @ {props.year} </h6>
+    </footer>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="Venkata Sandeep Kumar Karamsetty"/>
+      <Main adjective="Amazing"/>
+      <Footer year={new Date().getFullYear()}/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from "./App";
 
 
 
 ReactDOM.render(
-  React.createElement("h1", { style: { color: "blue"} }, "Venkata Sandeep Kumar. Karamsetty"),
+  <App />,
+  
   document.getElementById('root')
 );
 
