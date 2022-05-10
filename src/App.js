6774a5888ff55525a8d3a52d77c6fdce782c3645
venkata <sandeepsandy2530@gmail.com>
2022-05-10 21:00:30 +1000

    Adding context

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 import AuthContext from './store/auth-context';
-import { AuthContextProvider } from './store/auth-context';
 
 function App() {
 
@@ -13,13 +12,11 @@ function App() {
 
   return (
     <React.Fragment>
-      {/* <AuthContextProvider> */}
       <MainHeader  />
       <main>
         {!ctx.isLoggedIn && <Login  />}
         {ctx.isLoggedIn && <Home  />}
       </main>
-      {/* </AuthContextProvider> */}
     </React.Fragment>
   );
 }
