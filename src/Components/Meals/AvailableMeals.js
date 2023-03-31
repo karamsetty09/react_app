cefb25e57061b41e64efd41b3e00225fb43b825d
karamsetty09 <sandeepsandy2530@gmail.com>
2023-03-31 15:15:53 +1100

    cleanup and fix broken code.

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -3,33 +3,6 @@ import Card from '../UI/Card';
 import classes from './AvailableMeals.module.css'
 import MealItem from './MealItem/MealItem';
 
-const DUMMY_MEALS = [
-    {
-      id: 'm1',
-      name: 'Sushi',
-      description: 'Finest fish and veggies',
-      price: 22.99,
-    },
-    {
-      id: 'm2',
-      name: 'Schnitzel',
-      description: 'A german specialty!',
-      price: 16.5,
-    },
-    {
-      id: 'm3',
-      name: 'Barbecue Burger',
-      description: 'American, raw, meaty',
-      price: 12.99,
-    },
-    {
-      id: 'm4',
-      name: 'Green Bowl',
-      description: 'Healthy...and green...',
-      price: 18.99,
-    },
-  ];
-
 const AvailableMeals = () => {
     const [meals, setMeals] = useState([]);
     useEffect(() => {
@@ -51,6 +24,8 @@ const AvailableMeals = () => {
         setMeals(loadedMeals);
       };
 
+      fetchMeals();
+
     }, []);
     const mealsList = meals.map((meal) => (
     <MealItem 
@@ -68,7 +43,6 @@ const AvailableMeals = () => {
                 {mealsList}
             </ul>
           </Card>
-            
       </section>
   )
 }
